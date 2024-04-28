dcc8932c482915a5eb0eaf93f688318ae1e3ba22
Lorenzobattistela <lorenzobattistela@gmail.com>
2024-04-28 11:01:33 -0300

    training test file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 use std::collections::HashMap;
 
+use rusty_tokenizer::base::Tokenizer;
+use rusty_tokenizer::basic::{self, BasicTokenizer};
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 mod base;
 
 fn main() {
@@ -9,7 +15,7 @@ fn main() {
 
     let mut m = HashMap::new();
 
-    let mut i = vec![1, 2, 3, 1, 2];
+    let i = vec![1, 2, 3, 1, 2];
 
     m = base::get_stats(&i, Some(m));
     println!("{:?}", m);
@@ -28,4 +34,27 @@ fn main() {
     let token = b"abc\x1b[31mdef\x1b[0m";
     let result = base::render_token(token);
     println!("{}", result); // Output: abcdef
+
+    let file_path = "tests/taylorswift.txt";
+    let mut file = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("Error opening file: {}", err);
+            return;
+        }
+    };
+
+    let mut text = String::new();
+    if let Err(err) = file.read_to_string(&mut text) {
+        eprintln!("Error reading file: {}", err);
+        return;
+    }
+    println!("{}", text);
+
+    let mut tknz = BasicTokenizer::new();
+    println!("{:?}", tknz);
+    tknz.init_vocab();
+    // println!("{:?}", tknz);
+    tknz.train(text, 512, true);
+    println!("{:?}", tknz);
 }
